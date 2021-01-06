2048d95b9fba1aba77846dc3be800a1f1af85f89
OkiStuff <probro1802@gmail.com>
2021-01-06 13:55:51 -0500

    New Init Piko Function

--- include/Window.h
+++ include/Window.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+#include <glad/glad.h>
 #include "Vector.h"
 #include "util.h"
 #include "Sprite.h"
+
+
 
 
 // Kill Piko Engine Early
@@ -11,29 +13,87 @@
 
 typedef struct
 {
-    SDL_Window* win;
-    SDL_Renderer* renderer;
+    SDL_Window* window;
+    SDL_GLContext context;
 } Game;
 
 
-Game InitWindow(const char* p_title, int m_width, int m_height, Uint32 flags)
+Game InitPiko(const char* p_title, int m_width, int m_height)
 {
     if (SDL_Init(SDL_INIT_VIDEO) > 0) 
     {
         fprintf(stderr, "Failed to run SDL_Init Error %s", SDL_GetError());
         exit(1);
     }
-    if (!(IMG_Init(IMG_INIT_PNG))) 
-    {
-        fprintf(stderr, "Failed to Run IMG_INIT ERROR %s", SDL_GetError());
-        exit(1);
-    }
 
+    SDL_GL_LoadLibrary(NULL);
+
+    // Request an OpenGL 4.5 context (should be core)
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+    // Also request a depth buffer
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+
+
+    // Create Flags
     Game obj;
-    obj.win = SDL_CreateWindow(p_title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, m_width, m_height, SDL_WINDOW_SHOWN);
+    Uint32 flags;
+#ifdef PIKO_FULLSCREEN_ENABLED    
+    flags = SDL_WINDOW_FULLSCREEN_DESKTOP | SDL_WINDOW_OPENGL;
+#else 
+    flags = SDL_WINDOW_OPENGL;
+#endif
 
-    if (obj.win == NULL) printf("Failed to Create Window: %s", SDL_GetError());
-    obj.renderer = SDL_CreateRenderer(obj.win, -1, SDL_RENDERER_ACCELERATED);
+    
+
+    // Make Window
+    obj.window = SDL_CreateWindow(
+            p_title,
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            m_width, m_height,
+            flags
+            );
+
+    // Get OpenGL Context
+    obj.context = SDL_GL_CreateContext(obj.window);
+
+
+    // Error Handling
+
+    if (obj.context == NULL) printf("Failed to Create OpenGL Context: %s", SDL_GetError());
+    if (obj.window == NULL) printf("Failed to Create Window: %s", SDL_GetError());
+
+    printf("OpenGL loaded\n");
+    gladLoadGLLoader(SDL_GL_GetProcAddress);
+    printf("Vendor:   %s\n", glGetString(GL_VENDOR));
+    printf("Renderer: %s\n", glGetString(GL_RENDERER));
+    printf("Version:  %s\n", glGetString(GL_VERSION));
+
+    // Set V-Sync
+#ifdef PIKO_VSYNC_ENABLED
+    // Using V-Sync
+    SDL_GL_SetSwapInterval(1);
+#else
+    // Not Using V-Sync
+    SDL_GL_SetSwapInterval(0);
+#endif
+
+    // Disable depth test and face culling.
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_CULL_FACE);
+
+
+#ifdef PIKO_FULLSCREEN_ENABLED
+    int w,h;
+    SDL_GetWindowSize(obj.window, &w, &h);
+    glViewport(0,0,w,h);
+#else 
+    glViewport(0,0, m_width, m_height);
+#endif
+
     return obj;
 
     
@@ -41,17 +101,12 @@ Game InitWindow(const char* p_title, int m_width, int m_height, Uint32 flags)
 
 
 
-void pkDestroyWindow(Game win)
+void pkDestroyWindow(Game win);
+void ClearScreen()
 {
-    SDL_DestroyWindow(win.win);
+    glClearColor(0.0f, 0.5f, 1.0f, 0.0f);
 }
-
-void ClearScreen(SDL_Renderer* renderer)
+void UpdateDisplay(Game obj)
 {
-    SDL_RenderClear(renderer);
-}
-
-void UpdateDisplay(SDL_Renderer* renderer)
-{
-    SDL_RenderPresent(renderer);
+    SDL_GL_SwapWindow(obj.window);
 }
